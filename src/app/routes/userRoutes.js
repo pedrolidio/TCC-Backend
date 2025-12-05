c21df4addbb5c5f6898ca177a0c3a6de9901ff35
pedrolidio <pedrolidio@poli.ufrj.br>
2025-12-05 16:52:09 -0300

    Proteção de rotas privadas em userRoutes

--- src/app/routes/userRoutes.js
+++ src/app/routes/userRoutes.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
-
-// GET /api/users - Lista todos os usuários
-router.get("/", UserController.index);      
-
-// GET /api/users/:id - Exibe os dados de um usuário pelo seu id
-router.get("/:id", UserController.show);    
+const auth = require("../middlewares/auth");
+const checkRole = require("../middlewares/checkRole");
+const roles = require("../../config/roles");
 
 // POST /api/users - Cadastra um novo usuário
-router.post("/", UserController.create);    
+router.post("/", UserController.create);   
+
+// Rotas protegidas
+router.use(auth);
+
+// GET /api/users - Lista todos os usuários
+router.get("/", checkRole([roles.ADMIN]), UserController.index);      
+
+// GET /api/users/:id - Exibe os dados de um usuário pelo seu id
+router.get("/:id", checkRole([roles.ADMIN]), UserController.show);    
 
 module.exports = router;
