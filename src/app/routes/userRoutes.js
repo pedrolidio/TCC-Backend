850bd3726753265f204a035853093ca093a510ba
pedrolidio <pedrolidio@poli.ufrj.br>
2025-12-04 23:04:04 -0300

    Correção nos comentários

--- src/app/routes/userRoutes.js
+++ src/app/routes/userRoutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
 
-// GET /api/users - Lista todos os condutores
+// GET /api/users - Lista todos os usuários
 router.get("/", UserController.index);      
 
-// GET /api/users/:id - Exibe os dados de um condutor pelo seu id
+// GET /api/users/:id - Exibe os dados de um usuário pelo seu id
 router.get("/:id", UserController.show);    
 
-// POST /api/users - Cadastra um novo condutor
+// POST /api/users - Cadastra um novo usuário
 router.post("/", UserController.create);    
 
 module.exports = router;
